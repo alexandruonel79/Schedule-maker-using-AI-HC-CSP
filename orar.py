0458b9c153cf655b04e4fccd8401b0786d6c6330
alexandruonel79 <alexandruonel79@gmail.com>
2024-04-20 21:51:25 +0300

    Passes dummy but fails the others..

--- orar.py
+++ orar.py
@@ -2,8 +2,8 @@ from typing import Dict, Tuple, List
 import copy
 import random
 import sys
-from utils import read_yaml_file
-
+from utils import read_yaml_file, pretty_print_timetable
+from check_constraints import *
 MAXTEACHERINTERVALS = 7
 
 class State:
@@ -76,51 +76,95 @@ class State:
         Întoarce un generator cu toate posibilele stări următoare.
         '''
         next_states = []
+        #
+        # for subject, unallocated_students in self.remained_subjects.items():
+        #     print('subject', subject)
+        #     print('unallocated_students', unallocated_students)
+        #     # if i have any students left without the course
+        #     if int(unallocated_students) > 0:
+        #         # search for an available teacher
+        #         for teacher_name, left_intervals in self.remained_profs_intervals.items():
+        #             teacher_subjects_list = self.info_teacher_subjects[teacher_name]
+        #             if left_intervals and subject in teacher_subjects_list:
+        #                 # search for an available classroom
+        #                 for classroom, (classroom_capacity, classroom_subjects_list) in self.info_rooms.items():
+        #                     # classroom_subjects_list = self.info_rooms[classroom][1]
+        #                     # classroom_capacity = self.info_rooms[classroom][0]
+        #                     if subject in classroom_subjects_list and :
+        #                             for day in self.info_days:
+        #                                 for interval in self.info_intervals:
+        #
+        #                                     if day not in self.timetable:
+        #                                         self.timetable[day] = {}
+        #
+        #                                     if interval not in self.timetable[day]:
+        #                                         self.timetable[day][interval] = {}
+        #
+        #                                     if self.timetable[day][interval] == {}:
+        #                                         # we have the students, the teacher and the classroom
+        #                                         new_state = copy.deepcopy(self)
+        #                                         new_state.timetable[day][interval][classroom] = (teacher_name, subject)
+        #                                         new_state.remained_profs_intervals[teacher_name] -= 1
+        #                                         new_state.remained_subjects[subject] -= classroom_capacity
+        #                                         if new_state.remained_subjects[subject] < 0:
+        #                                             new_state.remained_subjects[subject] = 0
+        #                                         new_state.conflicts += self.is_soft_constraint(teacher_name, interval, day)
+        #                                         next_states.append(new_state)
 
-        for subject, unallocated_students in self.remained_subjects.items():
-            print('subject', subject)
-            print('unallocated_students', unallocated_students)
-            # if i have any students left without the course
-            if int(unallocated_students) > 0:
-                # search for an available teacher
-                for teacher_name, left_intervals in self.remained_profs_intervals.items():
-                    teacher_subjects_list = self.info_teacher_subjects[teacher_name]
-                    if left_intervals and subject in teacher_subjects_list:
-                        # search for an available classroom
-                        for classroom, (classroom_capacity, classroom_subjects_list) in self.info_rooms.items():
-                            # classroom_subjects_list = self.info_rooms[classroom][1]
-                            # classroom_capacity = self.info_rooms[classroom][0]
-                            if subject in classroom_subjects_list:
-                                    for day in self.info_days:
-                                        for interval in self.info_intervals:
+        for day in self.info_days:
+            for interval in self.info_intervals:
+                # copy the new state
+                new_state = copy.deepcopy(self)
+                # new_state.remained_subjects = copy.deepcopy(self.remained_subjects)
+                # new_state.remained_profs_intervals = copy.deepcopy(self.remained_profs_intervals)
 
-                                            if day not in self.timetable:
-                                                self.timetable[day] = {}
+                occupied_classrooms = {}
+                teacher_interval = []
 
-                                            if interval not in self.timetable[day]:
-                                                self.timetable[day][interval] = {}
+                if day not in new_state.timetable:
+                    new_state.timetable[day] = {}
 
-                                            if self.timetable[day][interval] == {}:
-                                                # we have the students, the teacher and the classroom
-                                                new_state = copy.deepcopy(self)
-                                                new_state.timetable[day][interval][classroom] = (teacher_name, subject)
-                                                new_state.remained_profs_intervals[teacher_name] -= 1
-                                                new_state.remained_subjects[subject] -= classroom_capacity
-                                                if new_state.remained_subjects[subject] < 0:
-                                                    new_state.remained_subjects[subject] = 0
-                                                new_state.conflicts += self.is_soft_constraint(teacher_name, interval, day)
-                                                next_states.append(new_state)
+                if interval not in new_state.timetable[day]:
+                    new_state.timetable[day][interval] = {}
+                else:
+                    continue
 
+                for subject, unallocated_students in new_state.remained_subjects.items():
+                    print('subject', subject)
+                    print('unallocated_students', unallocated_students)
+                    # if i have any students left without the course
+                    if int(unallocated_students) > 0:
+                        # search for an available teacher
+                        for teacher_name, left_intervals in new_state.remained_profs_intervals.items():
+                            teacher_subjects_list = new_state.info_teacher_subjects[teacher_name]
+                            if left_intervals and subject in teacher_subjects_list and teacher_name not in teacher_interval:
+                                teacher_interval.append(teacher_name)
+                                # search for an available classroom
+                                for classroom_name, (classroom_capacity, classroom_subjects_list) in new_state.info_rooms.items():
+                                    # classroom_subjects_list = self.info_rooms[classroom][1]
+                                    # classroom_capacity = self.info_rooms[classroom][0]
+                                    if subject in classroom_subjects_list and classroom_name not in occupied_classrooms:
+                                        occupied_classrooms[classroom_name] = (teacher_name, subject)
+                                        # update the new state
+                                        new_state.remained_profs_intervals[teacher_name] -= 1
+                                        new_state.remained_subjects[subject] -= classroom_capacity
+                                        if new_state.remained_subjects[subject] < 0:
+                                            new_state.remained_subjects[subject] = 0
+                                        new_state.conflicts += new_state.is_soft_constraint(teacher_name, interval, day)
+                # posibil necesar inca un deep copy
+                new_state.timetable[day][interval] = occupied_classrooms
+                next_states.append(copy.deepcopy(new_state))
         return next_states
 
 
 def eval_function(state: State) -> int:
     total_cost = 0
     remained_subjects = state.remained_subjects
+    # for _, students_count in initial_subjects_stud_count.items():
     for _, left_students_count in remained_subjects.items():
         if left_students_count != 0:
             total_cost += left_students_count * 100
-
+    # TODO DEOCAMDATA IGNOR ASTEA SOFT MUST FIX IS_SOFT AIA
     total_cost += state.conflicts
 
     return total_cost
@@ -152,7 +196,7 @@ def stochastic_hill_climbing(initial: State, max_iters: int = 1000):
             state = random.choice(better_states)
 
         if found_local_min:
-            print("Found local min")
+            print(str(state.is_final()) + " Found local min with score: " + str(eval_function(state)))
             return state.is_final(), iters, states, state
 
     return state.is_final(), iters, states, state
@@ -180,6 +224,29 @@ def create_input_structures(input_data):
 
     return info_days, info_intervals, info_teacher_subjects, info_teacher_constraints, info_rooms, remained_subjects, remained_profs_intervals
 
+def convert_string_to_int_tuple(timetable: Dict[str, Dict[str, Dict[str, Tuple[str, str]]]]) -> Dict[str, Dict[Tuple[int, int], Dict[str, Tuple[str, str]]]]:
+    new_timetable = {}
+    for day, intervals in timetable.items():
+        new_intervals = {}
+        for interval_str, courses in intervals.items():
+            start, end = map(int, interval_str.strip('()').split(','))
+            new_intervals[(start, end)] = courses
+        new_timetable[day] = new_intervals
+    return new_timetable
+
+def add_missing_intervals_and_rooms(state: State) -> State:
+    for day in state.timetable:
+        for interval in state.info_intervals:
+            if interval not in state.timetable[day]:
+                state.timetable[day][interval] = {}
+
+            for room in state.info_rooms:
+                if room not in state.timetable[day][interval]:
+                    state.timetable[day][interval][room] = None
+
+    return state
+
+
 def main():
     # ceva
     # Check if correct number of command-line arguments are provided
@@ -196,7 +263,6 @@ def main():
     # Read input file
     input_data = read_yaml_file(input_file)
     info_days, info_intervals, info_teacher_subjects, info_teacher_constraints, info_rooms, remained_subjects, remained_profs_intervals = create_input_structures(input_data)
-
     # Create initial state
     initial_state = State(
     info_days=info_days,
@@ -214,8 +280,20 @@ def main():
     if algorithm == 'hc':
         # TODO ADD DAYS TO THE TIMETABLE
         _, _, _, state = stochastic_hill_climbing(initial_state, 1000)
+        state = add_missing_intervals_and_rooms(state)
         print('Conflicte soft: ', state.conflicts)
         print(state.timetable)
+        print(pretty_print_timetable(convert_string_to_int_tuple(state.timetable), input_file))
+
+        print('\n----------- Constrângeri obligatorii -----------')
+        constrangeri_incalcate = check_mandatory_constraints(convert_string_to_int_tuple(state.timetable), input_data)
+
+        print(f'\n=>\nS-au încălcat {constrangeri_incalcate} constrângeri obligatorii!')
+
+        print('\n----------- Constrângeri optionale -----------')
+        constrangeri_optionale = check_optional_constraints(convert_string_to_int_tuple(state.timetable), input_data)
+
+        print(f'\n=>\nS-au încălcat {constrangeri_optionale} constrângeri optionale!\n')
 
 
 if __name__ == '__main__':
